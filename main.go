ea94b148f97529022565c3a4876b9fef4d1eb9eb
RB Station <15992567646@139.com>
2026-03-13 18:39:28 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+
+	"gcctool/internal/ui"
+)
+
+func main() {
+	// 全局错误恢复
+	defer func() {
+		if r := recover(); r != nil {
+			errMsg := fmt.Sprintf("程序异常:\n%v\n\n堆栈信息:\n%s", r, debug.Stack())
+			fmt.Fprintln(os.Stderr, errMsg)
+
+			// 尝试保存到文件
+			os.WriteFile("error.log", []byte(errMsg), 0644)
+
+			os.Exit(1)
+		}
+	}()
+
+	// 创建应用
+	app := ui.NewApp()
+
+	// 运行
+	app.Run()
+}
